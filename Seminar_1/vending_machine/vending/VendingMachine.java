7d8b09133bc3fa280f92842205d5df69a0244b5e
Ivan511440 <128604283+Ivan511440@users.noreply.github.com>
2023-08-01 11:26:06 +0600

    VendingMachine.

---
+++ Seminar_1/vending_machine/vending/VendingMachine.java
@@ -0,0 +1,30 @@
+package Seminar_1.vending_machine.vending;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Seminar_1.vending_machine.products.Product;
+
+public class VendingMachine {
+    private int productId;
+    private List<Product> productList;
+
+    public VendingMachine(int id) {
+        productList = new ArrayList<>();
+    }
+
+    public void addProduct(Product product){
+        product.setId(productId++);
+        productList.add(product);
+    }
+
+    public String getProductsInfo(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Список продуктов:\n");
+        for (Product product: productList){
+            stringBuilder.append(product);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
